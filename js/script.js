6c098959c0f0888ffbc4e380e23a2b7a84220e0c
TimShevy <126278688+TimShevy@users.noreply.github.com>
2023-04-06 16:32:37 +0500

    Добавил скрипт к PopUp

--- js/script.js
+++ js/script.js
@@ -19,3 +19,35 @@ var splide = new Splide( '.splide', {
 } );
 splide.mount();
 
+let wrapperPopUp__one = document.querySelector('.wrapperPopUp__one')
+let wrapperPopUp__two = document.querySelector('.wrapperPopUp__two')
+let linkEnterBtn = document.querySelector('.linkEnterBtn')
+let aReg = document.querySelector('.aReg')
+let aEnt = document.querySelector('.aEnt')
+
+linkEnterBtn.addEventListener('click', showPopUp__ent)
+aReg.addEventListener('click', showPopUp__reg)
+aEnt.addEventListener('click', showPopUp__ent)
+wrapperPopUp__one.addEventListener('click', closePopUp)
+wrapperPopUp__two.addEventListener('click', closePopUp)
+
+function showPopUp__ent() {
+  wrapperPopUp__two.classList.remove('active__wrapperPopUp')
+  wrapperPopUp__one.classList.add('active__wrapperPopUp')
+}
+function showPopUp__reg() {
+  wrapperPopUp__one.classList.remove('active__wrapperPopUp')
+  wrapperPopUp__two.classList.add('active__wrapperPopUp')
+}
+function closePopUp() {
+  let elem = event.target
+  if(elem == wrapperPopUp__one || elem == wrapperPopUp__two) {
+    wrapperPopUp__one.classList.remove('active__wrapperPopUp')
+    wrapperPopUp__two.classList.remove('active__wrapperPopUp')
+  }
+}
+
+
+
+
+
